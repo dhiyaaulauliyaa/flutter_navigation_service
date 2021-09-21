d22400f1f0eae7e32c7d3e4151b1f61173377840
Dhiyaaul Auliyaa (Ado) <ahmad.auliyaa@stockbit.com>
2021-09-21 20:09:07 +0700

    implement custom animation when navigating

--- lib/services/navigation.dart
+++ lib/services/navigation.dart
@@ -14,8 +14,19 @@ class NavigationService {
 
   Future<dynamic> push(Widget widget) async {
     return navigatorKey.currentState!.push(
-      MaterialPageRoute(
-        builder: (ctx) => widget,
+      // MaterialPageRoute(
+      //   builder: (ctx) => widget,
+      // ),
+      PageRouteBuilder(
+        pageBuilder: (c, a1, a2) => widget,
+        // settings: RouteSettings(name: routeName),
+        transitionsBuilder: (c, animation, a2, child) => SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1, 0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child),
+        transitionDuration: const Duration(milliseconds: 200),
       ),
     );
   }
