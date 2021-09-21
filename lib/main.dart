0a0be3c7d5c3d9469c5209eb9f197d23d73c8f8e
Dhiyaaul Auliyaa (Ado) <ahmad.auliyaa@stockbit.com>
2021-09-21 20:12:29 +0700

    move pages to dedicated folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'base/base_stateless.dart';
+
+import 'pages/pages.dart';
 import 'services/navigation.dart';
 
 void main() {
@@ -18,89 +19,6 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       home: FirstPage(),
-    );
-  }
-}
-
-class FirstPage extends BaseStateless {
-  FirstPage({Key? key}) : super(key: key);
-
-  @override
-  Widget body() {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Page'),
-      ),
-      body: const Center(
-        child: Text('First Page'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: 'p1',
-        onPressed: () {
-          nav.push(SecondPage());
-        },
-        child: const Icon(Icons.arrow_forward),
-      ),
-    );
-  }
-}
-
-class SecondPage extends BaseStateless {
-  SecondPage({Key? key}) : super(key: key);
-
-  @override
-  Widget body() {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Page'),
-      ),
-      body: const Center(
-        child: Text('Second Page'),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            heroTag: 'p2a',
-            onPressed: () {
-              nav.pop();
-            },
-            child: const Icon(Icons.arrow_back),
-          ),
-          const SizedBox(width: 16),
-          FloatingActionButton(
-            heroTag: 'p2b',
-            onPressed: () {
-              nav.push(ThirdPage());
-            },
-            child: const Icon(Icons.arrow_forward),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ThirdPage extends BaseStateless {
-  ThirdPage({Key? key}) : super(key: key);
-
-  @override
-  Widget body() {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Third Page'),
-      ),
-      body: const Center(
-        child: Text('Third Page'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: 'p3',
-        onPressed: () {
-          nav.pop();
-        },
-        child: const Icon(Icons.arrow_back),
-      ),
     );
   }
 }
